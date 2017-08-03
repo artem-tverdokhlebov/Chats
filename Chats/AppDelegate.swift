9d2e1bf2c81588b20c7962ae194b2a7479abfd8b
Artem Tverdokhlebov <artem.tverdokhlebov@gmail.com>
2017-08-03 11:34:10 +0300

    Cleanup

--- Chats/AppDelegate.swift
+++ Chats/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-//import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,8 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-//        IQKeyboardManager.sharedManager().enable = true
         
         UITabBar.appearance().unselectedItemTintColor = UIColor.white
         
